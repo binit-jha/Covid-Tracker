ee15361e9777841af69374cc1929cfbe1d725f66
Binit Jha <52690097+binit-jha@users.noreply.github.com>
2020-10-31 22:22:49 +0530

    Add files via upload

---
+++ covid.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 31 21:46:43 2020
+
+@author: BINIT KUMAR
+"""
+
+from covid import Covid
+
+import matplotlib.pyplot as plt
+
+covid= Covid()
+name=input("Enter the country name : ")
+
+virusdata = covid.get_status_by_country_name(name)
+
+remove=['id', 'country','latitude' ,'longitude', 'last_update']
+
+for i in remove:
+    virusdata.pop(i)
+all =virusdata.pop('confirmed')
+id =list(virusdata.keys())
+value = [str(i) for i in virusdata.values()]
+
+plt.pie(value, labels=id, colors =['r','y','g','b'] , autopct= '%1.1f%%')
+plt.title("COUNTRY : "+name.upper() +"\n TOTAL CASES : "+str(all))
+plt.legend()
+plt.show()
